7ea01ae062fddd031f757ed50bcbbc2148484261
sandipanghosh1993 <sandipan.ghosh1993s@gmail.com>
2017-06-22 03:26:20 +0530

    Socket IO implementation

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+var socket = io();
+
+socket.on('connect', () => {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', () => {
+  console.log('disconnected from server');
+});
+
+socket.on('newMessage', (message) => {
+  console.log('New message', message);
+});
