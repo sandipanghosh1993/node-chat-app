e017d4d3d1db826dd61c0259a9d756b1f3481365
sandipanghosh1993 <sandipan.ghosh1993s@gmail.com>
2017-06-23 01:26:58 +0530

    add form for messages and show it on browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,19 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
   console.log('New message', message);
+  var li=jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#message').append(li);
+});
+
+jQuery('#message-form').on('submit', (e) => {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, () => {
+
+  });
 });
